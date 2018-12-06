23d807ac4a123736db6b3766e08446f5a3a7b891
ankush banik <ankushbanik1234@gmail.com>
2018-12-06 19:09:11 +0530

    opencv day 1

---
+++ opencv/opencv1.py
@@ -0,0 +1,8 @@
+import cv2
+img= cv2.imread('C:/Users/ankush\OneDrive/redmi 5 my phone data/sd card xender pics/IMG_20170614_162314.jpg')
+cv2.imshow('output show',img)
+
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
